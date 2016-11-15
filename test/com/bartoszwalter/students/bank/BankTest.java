170d0d0706ac8487b2aca5422ff8f45fc3912a59
Bartek Walter <bartek@walterowie.pl>
2016-11-15 15:33:32 +0100

    initial commit

---
+++ test/com/bartoszwalter/students/bank/BankTest.java
@@ -0,0 +1,38 @@
+package com.bartoszwalter.students.bank;
+
+import com.bartoszwalter.students.bank.Bank;
+import com.bartoszwalter.students.bank.Rachunek;
+
+import junit.framework.TestCase;
+
+public class BankTest extends TestCase {
+
+	Bank bank = null;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		bank = new Bank();
+	}
+
+	public void testZalozRachunek() {
+		Rachunek rachunek = bank.zalozRachunek("111", "Jan", "Kowalski");
+		assertEquals("111", rachunek.numer());
+		
+		rachunek = bank.szukaj("111");
+		assertNotNull(rachunek);
+		assertEquals("111", rachunek.numer());
+	}
+
+	public void testSzukaj() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrzelewStringStringInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrzelewRachunekRachunekInt() {
+		fail("Not yet implemented");
+	}
+
+}
